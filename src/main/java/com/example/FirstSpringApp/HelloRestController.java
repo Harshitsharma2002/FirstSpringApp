dcde732be7d2a87d424d8b4c202019d2a237c9ff
Harshit-sharma-cs21 <hrsharma8171087251@gmail.com>
2025-03-03 11:27:29 +0530

    Added

--- src/main/java/com/example/FirstSpringApp/HelloRestController.java
+++ src/main/java/com/example/FirstSpringApp/HelloRestController.java
@@ -17,5 +17,9 @@ public class HelloRestController {
     public String sayHelloParam(@PathVariable String name) {
         return "Hello " + name + "!";
     }
+    @PostMapping("/post")
+    public String sayHello(@RequestBody User user) {
+        return "Hello " + user.getFirstName() + " " + user.getLastName() + "!";
+    }
 
 }
